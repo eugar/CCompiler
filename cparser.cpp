daa13cd46c0d5e442fb79095beb55715d9266225
Victor Gutierrez <victorgutierrez5701@gmail.com>
2019-03-25 22:51:36 -0600

    Action and Goto access methods

--- cparser.cpp
+++ cparser.cpp
@@ -2,11 +2,25 @@
 // created by Victor Gutierrez on Mar 25, 2019
 //
 #include <iostream>
-#include <cparser.hpp>
+#include "cparser.hpp"
 
 using namespace std;
 
 Parser::Parser()
 {
 
+}
+
+pair<StateTables::Action::ACTION, size_t> Parser::getAction(size_t currentState, string token)
+{
+    auto act = m_stateTable.m_action.table.find(currentState);
+
+    return act->second.find(token)->second;
+}
+
+size_t Parser::getGoto(size_t currentState, string token)
+{
+    auto gt = m_stateTable.m_goto.table.find(currentState);
+
+    return gt->second.find(token)->second;
 }
--- cparser.hpp
+++ cparser.hpp
@@ -4,12 +4,25 @@
 #ifndef CCOMPILER_CPARSER_H
 #define CCOMPILER_CPARSER_H
 
+#include "statetables.h"
+#include <vector>
+
+using namespace std;
+
 class Parser
 {
 public:
     Parser();
-private:
+    void printParseTree();
 
+private:
+    pair<StateTables::Action::ACTION, size_t> getAction(size_t currentState, string token);
+    size_t getGoto(size_t currentState, string token);
+    void setState(size_t newState){m_currentState = newState;}
+
+    size_t m_currentState;
+    StateTables m_stateTable;
+    vector<string> m_stateStack;
 };
 
 #endif //CCOMPILER_CPARSER_H
