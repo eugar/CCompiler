e2da3aa56a94bcbdde4a6f90a150a0e0d40b0876
Victor Gutierrez <victorgutierrez5701@gmail.com>
2019-04-24 06:41:52 -0600

    Messing with mutables

--- simple.c
+++ simple.c
@@ -10,11 +10,10 @@ int a = 5;
 int main ()
 {
     int z = 7 + 2 * 6;
-
     //int b = 2 * 3 + 4;
     //char a = 'v';         // x86 gen doesn't recognize chars ?
-    //int g = 6 / 3;        // x86 gen doesn't recognize DIV ?
+    int g = 6 / 3;        // x86 gen doesn't recognize DIV ?
     //g = 5;                // not working in the IR gen yet!!
 
-    return z;
+    return z+5*6;
 }
