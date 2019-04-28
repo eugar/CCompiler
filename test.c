2e3c1b74910f499b0679a5d103c114638fd82084
Eugene <egarcia@cs.nmt.edu>
2019-04-27 19:09:42 -0600

    Push test file

--- test.c
+++ test.c
@@ -1,4 +1,24 @@
-int main() {
-    int z = 6 * 7 + 5 + 3 + 1 + 4 + 5 + 6 + 5 + 2;
+int fun()
+{
+    int v = 10 + 9;
+
+    return v;
+}
+
+int main ()
+{
+    int z = 1 * 2 + 3 * 4 * 5;
+
+    int b = 3 - 1;
+
+    if (b < 10)
+    {
+        return 10;
+    }
+
+    int g = 6 / 3 + 5 * 3;
+    g = 5;
+    z = g;
+
     return z;
 }
