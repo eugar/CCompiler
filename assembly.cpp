1e8f0b16cd725c89963aac0c9f8a4e308d2937ca
Eugene <egarcia@cs.nmt.edu>
2019-05-01 10:31:42 -0600

    Fix assembly spacing.

--- assembly.cpp
+++ assembly.cpp
@@ -60,7 +60,7 @@ void Assembly::generateCode(vector<irInstruction> instructions)
 
 void Assembly::insertBB(irInstruction ins)
 {
-    if (ins.op == "FUNC" || ins.op == "LABEL")
+    if (ins.op == "FUNC" || ins.op == "LABEL" || !(ins.block.empty()))
     {
         out << "#.bb" << this->bbcount << ":" << endl;
         bbcount++;
@@ -233,7 +233,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
             }
             else
             {
-                writeInstruction("jne\t\t" + this->gotoString);
+                writeInstruction("jne \t\t" + this->gotoString);
             }
         }
         else if (ins.op == "NOTEQ")
@@ -250,7 +250,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
             }
             else
             {
-                writeInstruction("je\t\t" + this->gotoString);
+                writeInstruction("je  \t\t" + this->gotoString);
             }
 
         }
@@ -268,7 +268,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
             }
             else
             {
-                writeInstruction("jge\t\t" + this->gotoString);
+                writeInstruction("jge \t\t" + this->gotoString);
             }
         }
         else if (ins.op == "GRTH")
@@ -285,7 +285,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
             }
             else
             {
-                writeInstruction("jle\t\t" + this->gotoString);
+                writeInstruction("jle \t\t" + this->gotoString);
             }
         }
         else if (ins.op == "GREQ")
@@ -302,7 +302,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
             }
             else
             {
-                writeInstruction("jl\t\t" + this->gotoString);
+                writeInstruction("jl  \t\t" + this->gotoString);
             }
 
         }
@@ -320,7 +320,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
             }
             else
             {
-                writeInstruction("jg\t\t" + this->gotoString);
+                writeInstruction("jg  \t\t" + this->gotoString);
             }
 
         }
@@ -343,7 +343,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
         }
         else
         {
-            writeInstruction("je\t\t" + gotoString);
+            writeInstruction("je  \t\t" + gotoString);
         }
     }
     else if (ins.op == "OR")
@@ -352,7 +352,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
 
         writeInstruction("movb\t\t" + createString(ins.arg1) + ", %bl");
         writeInstruction("movb\t\t" + createString(ins.arg2) + ", %bh");
-        writeInstruction("or\t\t%bh, %bl");
+        writeInstruction("or  \t\t%bh, %bl");
 
         if (this->orFlag || this->andFlag)
         {
@@ -398,7 +398,7 @@ void Assembly::chooseInstruction(irInstruction ins) {
     }
     else if (ins.op == "JMP")
     {
-        writeInstruction("jmp\t\t" + ins.res);
+        writeInstruction("jmp \t\t" + ins.res);
     }
     else if (ins.op == "LABEL")
     {
